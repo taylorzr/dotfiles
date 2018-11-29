75fa41e707452f3f791647e94deb1620a4357e60
Zach Taylor <taylorzr@gmail.com>
2018-11-29 09:41:34 -0600

    Add timer to prompt

--- .config/shell/prompt.sh
+++ .config/shell/prompt.sh
@@ -1,4 +1,8 @@
-prompt_command() {
+function preexec() {
+  timer_start=${timer_start:-$SECONDS}
+}
+
+function precmd() {
   local last_exit_code=$?
 
   local git_branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/')
@@ -7,8 +11,14 @@ prompt_command() {
     git_branch="%F{yellow}${git_branch}%f"
   fi
 
+  if [ $timer_start ]; then
+    command_time=$(($SECONDS - $timer_start))
+    RPROMPT="${command_time}s"
+    unset timer_start
+  fi
+
   PS1="%1d"
-  RPROMPT="${git_branch}"
+  RPROMPT+="${git_branch}"
 
   if [ "${last_exit_code}" -ge 1 ]; then
     if [[ $(uname -s) == Linux ]]; then
@@ -18,11 +28,10 @@ prompt_command() {
       say oops
     fi
     PS1+=" %F{red}:(%f "
-    RPROMPT=" %F{red}Exit Code: ${last_exit_code}%f$RPROMPT"
+    RPROMPT=" %F{red}Exit Code: ${last_exit_code}%f $RPROMPT"
   else
     PS1+=" %F{green}:)%f "
     tput bel
   fi
 }
 
-func precmd() { prompt_command; }
