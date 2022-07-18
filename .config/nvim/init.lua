58fecce5279910d1f57c7d3a88de3bac07171c66
Zach Taylor <zach@taylorzr.com>
2022-07-17 21:28:11 -0400

    More config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,44 @@
 require('plugins')
 
-vim.g.mapleader = ' '
-vim.keymap.set("n", "<leader>q", "<cmd>bd<cr>")
-vim.keymap.set("n", "<leader>Q", "<cmd>qa<cr>")
-vim.keymap.set({"n", "i"}, "<C-s>", "<cmd>write<cr>")
+-- TODO
+-- test octo, see docs https://github.com/pwntester/octo.nvim#-examples
+-- maybe switch cmp accept to tab, using enter is goofy when you're trying to just insert a newline
+-- maybe suda for sudo writing https://github.com/lambdalisue/suda.vim/
 
+-- use system clipboard for yank/paste
+if vim.fn.has('macunix') then
+	vim.opt.clipboard = 'unnamedplus'
+else
+	vim.opt.clipboard = 'unnamed'
+end
+
+vim.g.mapleader = ' '
+
+-- this might be a stupid setting, but i'm trying to diff between tabs and spaces
+vim.opt.tabstop = 3
+vim.opt.shiftwidth = 3
+vim.opt.textwidth = 100
+
+-- keymaps
+vim.keymap.set("n", "<leader>q", "<cmd>bd<cr>")
+vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>write<cr>")
+-- terminal like begin/end line in insert mode
 vim.keymap.set("i", "<C-a>", "<C-o>^")
 vim.keymap.set("i", "<C-e>", "<C-o>$")
+-- easier in/dedent
+vim.keymap.set("n", ">", ">>")
+vim.keymap.set("n", "<", "<<")
+vim.keymap.set("n", "<leader>f", "<cmd>Lexplore<CR>")
 
--- move this into config block for plugin?
--- it wasn't working for some reason though
--- :LspInfo in a go file should show a client connected
+
+-- languag specific
+-- TODO autoimports for golang at least
+-- it kinda works if you type a package name it adds it
+-- but if you remove a package it doesn't pull it out
 vim.cmd('autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync(nil, 1000)')
-vim.cmd('autocmd BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 1000)')
--- vim.cmd('autocmd FileType go setlocal omnifunc=v:lua.vim.lsp.omnifunc')
--- TODO autoimports
+-- FIXME: autofmt lua keeps asking for which language server
+-- vim.cmd('autocmd BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 1000)')
+
+vim.cmd('autocmd Filetype rb lua vim.opt_local.expandtab = true')
+
+
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,17 @@
-return require('packer').startup(function()
+return require('packer').startup(function(use)
 	use{
-		'wbthomason/packer.nvim',
+		"catppuccin/nvim",
+		as = "catppuccin",
 		config = function()
+			require("catppuccin").setup()
+			vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+			vim.cmd[[colorscheme catppuccin]]
+		end
+	}
+	use {
+		'wbthomason/packer.nvim',
 
+		config = function()
 			vim.cmd([[
 			augroup packer_user_config
 			autocmd!
@@ -12,7 +21,13 @@ return require('packer').startup(function()
 
 		end
 	}
- 
+
+	-- lines for each indent level
+	use "lukas-reineke/indent-blankline.nvim"
+
+	-- helpers for file commands like mv, rm, chmod
+	use 'tpope/vim-eunuch'
+
 	use 'L3MON4D3/LuaSnip'
 	use {
 		'saadparwaiz1/cmp_luasnip',
@@ -32,16 +47,23 @@ return require('packer').startup(function()
 	use 'hrsh7th/cmp-path'
 	use 'hrsh7th/cmp-nvim-lua'
 	use 'hrsh7th/cmp-nvim-lsp'
-	use {
-		'hrsh8th/nvim-cmp',
-		config = function()
 
-			vim.opt.completeopt = {"menu", "menuone", "noselect"}
-			local cmp = require'cmp'
+	use {
+		'hrsh7th/nvim-cmp',
+		config = function()
+			vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local has_words_before = function()
+			  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
 			cmp.setup({
 				snippet = {
 					expand = function(args)
-						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+						require('luasnip').lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -49,17 +71,38 @@ return require('packer').startup(function()
 					-- documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
+					-- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+					-- ['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif cmp.visible() then
+							cmp.confirm({ select = true }) -- ({ select = true }) maybe?
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' }, -- For luasnip users.
-					{ name = 'buffer' }
-				})
+					{ name = 'luasnip' },
+					{ name = 'nvim_lsp_signature_help' },
+					{ name = 'nvim_lua' },
+					{ name = 'buffer' },
+				}),
+				experimental = {
+					ghost_text = true
+				}
 			})
 
 			-- Set configuration for specific filetype.
@@ -77,34 +120,60 @@ return require('packer').startup(function()
 		'neovim/nvim-lspconfig',
 		config = function()
 			local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local on_attach = function()
+				-- print("hello bro") -- used to test if this is being loaded
+				-- see options with :h vim.lsp.buf....
+				-- buffer = 0 means set for current buffer
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+				vim.keymap.set("n", "gn", vim.lsp.diagnostic.goto_next, { buffer = 0 })
+				vim.keymap.set("n", "gp", vim.lsp.diagnostic.goto_prev, { buffer = 0 })
+				vim.keymap.set("n", "gl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
+				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
+				vim.keymap.set("n", "gR", vim.lsp.buf.rename, { buffer = 0 })
+				vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = 0 })
+			end
 
-			require'lspconfig'.gopls.setup {
+			require 'lspconfig'.gopls.setup {
 				capabilities = capabilities,
+				on_attach = on_attach
+			}
 
-				on_attach = function()
-					-- print("hello bro")
-					-- see options with :h vim.lsp.buf....
-					-- buffer = 0 means set for current buffer
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-					vim.keymap.set("n", "gn", vim.lsp.diagnostic.goto_next, { buffer = 0 })
-					vim.keymap.set("n", "gp", vim.lsp.diagnostic.goto_prev, { buffer = 0 })
-					vim.keymap.set("n", "gl", "<cmd>Telescope diagnostics<cr>", { buffer = 0 })
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
-					vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = 0 })
-					vim.keymap.set("n", "gR", vim.lsp.buf.rename, { buffer = 0 })
-					vim.keymap.set("n", "ga", vim.lsp.buf.code_action, { buffer = 0 })
+			require'lspconfig'.terraformls.setup{}
 
-				end
+			require 'lspconfig'.sumneko_lua.setup {
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+							version = 'LuaJIT',
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = { 'vim' },
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						-- Do not send telemetry data containing a randomized but unique identifier
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
 			}
 		end
-}
+	}
 
 	use {
 		'alexghergh/nvim-tmux-navigation',
 		config = function()
-			require'nvim-tmux-navigation'.setup {
+			require 'nvim-tmux-navigation'.setup {
 				disable_when_zoomed = true, -- defaults to false
 				keybindings = {
 					left = "<C-h>",
@@ -114,17 +183,17 @@ return require('packer').startup(function()
 					last_active = "<C-\\>",
 				}
 			}
-		end 
+		end
 	}
 
-	use { 
+	use {
 		'nvim-treesitter/nvim-treesitter',
 		run = ':TSUpdate',
 		config = function()
 
-			require'nvim-treesitter.configs'.setup {
+			require 'nvim-treesitter.configs'.setup {
 				-- A list of parser names, or "all"
-				ensure_installed = {  "lua", "go", "ruby" },
+				ensure_installed = { "lua", "go", "ruby" },
 
 				-- Automatically cmpinstall missing parsers when entering buffer
 				auto_install = true,
@@ -153,7 +222,7 @@ return require('packer').startup(function()
 	use {
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.0',
-		requires = {{'nvim-lua/plenary.nvim'}},
+		requires = { { 'nvim-lua/plenary.nvim' } },
 		config = function()
 			vim.cmd('nnoremap <C-p> <cmd>Telescope find_files<cr>')
 			vim.cmd('nnoremap <leader>ff <cmd>Telescope find_files<cr>')
@@ -168,7 +237,7 @@ return require('packer').startup(function()
 		'mfussenegger/nvim-dap',
 		config = function()
 
-			local dap=require('dap')
+			local dap = require('dap')
 			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
 			vim.keymap.set("n", "<leader>c", dap.continue)
 			vim.keymap.set("n", "<leader>do", dap.repl.open)
@@ -216,15 +285,23 @@ return require('packer').startup(function()
 		"rafamadriz/friendly-snippets",
 		config = function()
 			require("luasnip.loaders.from_vscode").lazy_load()
-			require'luasnip'.filetype_extend("go", {"go"})
-			require'luasnip'.filetype_extend("ruby", {"rails"})
+			require 'luasnip'.filetype_extend("go", { "go" })
+			require 'luasnip'.filetype_extend("ruby", { "rails" })
 		end
 	}
 
 	use 'machakann/vim-sandwich'
+
+
+	use {
+		'pwntester/octo.nvim',
+		requires = {
+			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope.nvim',
+			'kyazdani42/nvim-web-devicons',
+		},
+		config = function ()
+			require"octo".setup()
+		end
+}
 end)
-
-
-
-
-
