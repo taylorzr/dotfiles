0561d111b76f17291ffbb9b7b22c132ebd3312b3
Zach Taylor <108883+taylorzr@users.noreply.github.com>
2023-03-22 21:23:59 -0400

    replace packer with lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,3 @@
-require('plugins')
 
 -- TODO
 -- test octo, see docs https://github.com/pwntester/octo.nvim#-examples
@@ -184,3 +183,17 @@ else
   vim.keymap.set("n", "<leader>cl",
     "<cmd>let @+=expand('%') . ':' . line('.') | echo 'filename:line copied to clipboard!'<CR>")
 end
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require('plugins')
