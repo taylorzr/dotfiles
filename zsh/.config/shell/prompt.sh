c1777cc308c7e3d19ba82cf653c73bf5ff727c14
Zach Taylor <108883+taylorzr@users.noreply.github.com>
2024-12-26 08:44:02 -0500

    toggle oops

--- zsh/.config/shell/prompt.sh
+++ zsh/.config/shell/prompt.sh
@@ -40,7 +40,6 @@ function precmd() {
     PS1="%F{yellow}$git_project%f$git_path" # root of project and path
     PS1+=" %F{yellow}git:${git_branch}(${git_sha})%f" # branch and sha
   else
-  # PS1="%1d"
     PS1="%/"
   fi
 
@@ -48,19 +47,9 @@ function precmd() {
     PS1+=" %F{blue}tf:$tf_version:$tf_workspace%f"
   fi
 
-  # postgres_version=$(psql -V | cut -f 3 -d ' ' | cut -f 1,2 -d .)
-  # PS1+=" pg(${postgres_version})"
-
   if [ "${last_exit_code}" -ge 1 ]; then
-    # FIXME: Make this global, maybe write to a file or something
-    if [ "$NO_OOPS" != 'true' ]; then
-      if [[ $(uname -s) == Linux ]]; then
-        echo oops | espeak 2> /dev/null # TODO: Async here would be nice tho
-      else
-        # TODO: Async oops, meh this seems ok on osx
-        # TODO: Allow disabling this
-        say oops
-      fi
+    if [ -f "$OOPS_FILE" ]; then
+      say oops
     fi
     PS1="%F{red}:(%f $PS1 %F{red}${command_time}s ${last_exit_code}%f"
   else
@@ -69,4 +58,25 @@ function precmd() {
   fi
 
   PS1="$PS1"$'\n'"$ "
+}
+
+export OOPS_FILE="$HOME/.oops"
+
+function oops() {
+  if [ -f "$OOPS_FILE" ]; then 
+    rm "$OOPS_FILE"
+    say 'oops disabled'
+  else
+    touch "$OOPS_FILE"
+    say 'oops enabled'
+  fi
+}
+
+function say() {
+  phrase="${1:-oops}"
+  if [[ $(uname -s) == Linux ]]; then
+    eval "$(<<< "$phrase" espeak 2>&1 > /dev/null &)"
+  else
+    command say "$phrase"
+  fi
 }
